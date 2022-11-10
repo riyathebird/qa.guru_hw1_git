02249e021207cd3714d9078e347e88b66f9a82f2
Katja Sannikova <sannikova.k@gmail.com>
2022-11-10 23:05:02 +0300

    removed all data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi");
-        System.out.println("Bye");
+        System.out.println("Nothing is here");
     }
 }
